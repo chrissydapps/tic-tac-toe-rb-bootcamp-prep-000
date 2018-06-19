4e0962bcffad1ac903cc5aa6a81a80146882305f
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 09:39:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -51,8 +51,19 @@ def turn(board)
 end
 
 def turn_count(board)
-  
+  number_of_turns = 0
+  board.each do |space|
+    if space == "X" || space == "O"
+      number_of_turns += 1
+    end
+  end
+  number_of_turns
 end
+
+def current_player(board)
+  (turn_count(board) % 2 == 0) ? "X" : "O"
+end
+
 
 def won?(board)
   WIN_COMBINATIONS.detect do |position|
