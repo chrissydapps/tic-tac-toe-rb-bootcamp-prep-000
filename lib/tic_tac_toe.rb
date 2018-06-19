748bc9416c6c7f07a434942d63d729f323a4bb42
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 10:15:27 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,7 +39,7 @@ def turn(board)
   index = input_to_index(input)
   if valid_move?(board, index)
     move(board, index)
-    display_board(board)
+    #display_board(board)
   else
     until valid_move?(board, index)
       puts "Please enter 1-9:"
