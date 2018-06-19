5b7da3dbcc40eb9e2a36ad333c9b3c33bc2dbff5
Christabel Dapaa-Addo <cdapaaaddo@gmail.com>
2018-06-19 09:38:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,6 +43,7 @@ def turn(board)
   else
     until valid_move?(board, index)
       puts "Please enter 1-9:"
+      turn(board)
       input = gets.strip
       index = input_to_index(input)
     end
